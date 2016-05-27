9995ee5d486f1c9e84ce113f9c0c2dc876dad1a5
Oscar "Xeno" Marti <oscarsanes9@gmail.com>
2016-05-27 22:47:44 +0200

    Update cl_cpda.lua

--- plugin/derma/cl_cpda.lua
+++ plugin/derma/cl_cpda.lua
@@ -26,20 +26,21 @@ antialias = true,
 shadow = false
 } )
 
-function PANEL:vgui.create("DFrame", panel)
+function PANEL:vgui.create("DFrame")
 	self:SetSize(380, 720);
 	self:SetPos(center);
 	self:SetBackgroudColor(255,255,255);
 	self:SetTitle("Combine PDA");
 end
 
-local Button1 = vgui.Create( "DButton" )	
+local Button1 = vgui.Create( "DButton", frame )	
 	Button1:SetFont( "FrameDP" )		
-	Button1:SetText = ("")
+	Button1:SetText = ("Hola")
 	Button1:SetPos( 20 , 50 )				
 	Button1:SetSize( 100, 50 )				
 	Button1.DoClick = function()			
 	local Frame1 = vgui.Create("")
+		print( "Button was clicked!" )
 end
 
 

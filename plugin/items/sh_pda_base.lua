d2e5f3eb9897f73f2ee21d0ca7db38d294c51af7
SellMG <SellMG@users.noreply.github.com>
2016-05-29 00:05:37 +0200

    Create sh_pda_base.lua

---
+++ plugin/items/sh_pda_base.lua
@@ -0,0 +1,60 @@
+--[[
+	© 2016 Sell & Xeno
+--]]
+
+ITEM = Clockwork.item:New(nil, true);
+ITEM.name = "PDA Base";
+ITEM.weight = 0.1;
+ITEM.access = "3";
+ITEM.category = "Tecnologia";
+
+-- Called when a player uses the item.
+function ITEM:OnUse(player, itemEntity)
+	local trace = player:GetEyeTraceNoCursor();
+	
+	if (trace.HitPos:Distance( player:GetShootPos() ) <= 192) then
+		local entity = ents.Create("cw_pda");
+		
+		Clockwork.player:GiveProperty(player, entity);
+		
+		entity:SetModel(self.model);
+		entity:SetBook(self.uniqueID);
+		entity:SetPos(trace.HitPos);
+		entity:Spawn();
+		
+		if (IsValid(itemEntity)) then
+			local physicsObject = itemEntity:GetPhysicsObject();
+			
+			entity:SetPos( itemEntity:GetPos() );
+			entity:SetAngles( itemEntity:GetAngles() );
+			
+			if (IsValid(physicsObject)) then
+				if (!physicsObject:IsMoveable()) then
+					physicsObject = entity:GetPhysicsObject();
+					
+					if (IsValid(physicsObject)) then
+						physicsObject:EnableMotion(false);
+					end;
+				end;
+			end;
+		else
+			Clockwork.entity:MakeFlushToGround(entity, trace.HitPos, trace.HitNormal);
+		end;
+	else
+		Clockwork.player:Notify(player, "No puedes dropear una pda por ahi!");
+		
+		return false;
+	end;
+end;
+
+-- Called when the item should be setup.
+function ITEM:OnUse(player, itemEntity)
+	if ITEM.uniqueID = "cw_utcpda" then
+		Clockwork.datastream:Start(player, "upda", entity);
+end;
+
+if ITEM.uniqueID = "cw_cpcpda"  then
+	Clockwork.datastream:Start(player, "cpda", entity);
+end;
+
+ITEM:Register();

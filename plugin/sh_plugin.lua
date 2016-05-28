cecc93974e437e442e37f0eae5a2557f4dd3279c
SellMG <SellMG@users.noreply.github.com>
2016-05-29 01:01:18 +0200

    Update sh_plugin.lua

--- plugin/sh_plugin.lua
+++ plugin/sh_plugin.lua
@@ -1,6 +1,6 @@
 local PLUGIN = PLUGIN;
 
-Schema:AddCustomPermit("Tecnologia", "4", "models/gibs/shield_scanner_gib1.mdl");
+Schema:AddCustomPermit("Tecnologia", "3", "models/gibs/shield_scanner_gib1.mdl");
 
 Clockwork.kernel:IncludePrefixed("cl_hooks.lua");
 Clockwork.kernel:IncludePrefixed("sv_hooks.lua");
